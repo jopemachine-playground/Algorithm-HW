f77b3ae483df29640258be49e4e2804daff6d3fb
jopebot <jopemachine@naver.com>
2019-11-09 18:15:02 +0900

    Update knapback.py

--- Assign08/knapback.py
+++ Assign08/knapback.py
@@ -1,38 +1,59 @@
 class Item:
 
     def __init__(self, value, weight):
-        self.__value = value
-        self.__weight = weight
+        self.value = value
+        self.weight = weight
 
 
-def pack(allItems, cache, capacity, index, itemMaxNumber):
-    if index == itemMaxNumber:
+# 원래 알고 있던 방법으로 푼 것
+def pack(allItems, capacity, index):
+    if index == pack.itemMaxNumber:
         return 0
 
-    ret = cache[capacity][index]
+    if pack.cache[capacity][index] != -1:
+        return pack.cache[capacity][index]
 
-    if ret != -1 :
-        return ret
-
-    ret = pack(allItems, cache, capacity, index + 1, itemMaxNumber)
+    pack.cache[capacity][index] = pack(allItems, capacity, index + 1)
 
     if capacity >= allItems[index].weight:
-        ret = max(ret, pack(allItems, cache, capacity - allItems[index].weight, index + 1, itemMaxNumber) + allItems[index].value)
+        pack.cache[capacity][index] = max(pack.cache[capacity][index],
+                                          pack(allItems,
+                                               capacity - allItems[index].weight,
+                                               index + 1) + allItems[index].value)
 
-    return ret
+    return pack.cache[capacity][index]
+
+
+# 의사코드를 이용해 짠 코드
+def pack_2(allItems, capacity, index):
+    if index == -1:
+        return 0
+
+    if pack_2.opt[index][capacity] != -1:
+        return pack_2.opt[index][capacity]
+
+    if allItems[index].weight > capacity:
+        pack_2.opt[index][capacity] = pack_2(allItems, capacity, index - 1)
+    else:
+        v1 = pack_2(allItems, capacity, index - 1)
+        v2 = allItems[index].value + pack_2(allItems, capacity - allItems[index].weight, index - 1)
+
+        pack_2.opt[index][capacity] = max(v1, v2)
+
+    return pack_2.opt[index][capacity]
 
 
 if __name__ == "__main__":
 
     source_data = "data09_knapsack.txt"
 
-    print("배낭의 사이즈를 입력하세요: ")
+    print("배낭의 사이즈를 입력하세요 (0~50) : ")
     bag_size = input()
 
     try:
-        item_cnt = 0
+        item_cnt = -1
         allItems = list()
-        cache = list(list())
+
         fr = open(source_data, 'r')
         while True:
             item_cnt = item_cnt + 1
@@ -40,8 +61,31 @@ if __name__ == "__main__":
             if not line:
                 break
             index, value, weight = line.split(",")
-            allItems.append(Item(value, weight))
-        print(pack(allItems, cache, bag_size, 0, item_cnt))
+            allItems.append(Item(int(value), int(weight)))
+
+        pack_2.selectItem = list()
+        pack_2.opt = [[-1] * (int(bag_size) + 1) for i in range(0, item_cnt + 1)]
+
+        # 테이블에 있는 값들 출력
+        for i in range(0, item_cnt):
+            for j in range(0, int(bag_size) + 1):
+                print(str(pack_2(allItems, j, i)), end='    ')
+            print()
+
+        pack_2.selectItem = list()
+        pack_2.opt = [[-1] * (int(bag_size) + 1) for i in range(0, item_cnt + 1)]
+
+        # 주어진 knapback 문제를 풀고 max 값을 찾아냄
+        maxValue = pack_2(allItems, int(bag_size), int(item_cnt) - 1);
+        print("max : " + str(maxValue))
+
+        # 주어진 문제에서 고른 item 들을 찾아 출력
+        ans = list()
+        for i in range(0, item_cnt):
+            for j in range(0, int(bag_size) + 1):
+                if pack_2.opt[i][j] == maxValue:
+                    ans.append(i)
+        print(ans)
 
     except FileNotFoundError:
         print("test File Not Found!")
