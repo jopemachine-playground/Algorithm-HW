868679f4c4845f12c6c3c19c4f86f42b5b1ad558
jopebot <jopemachine@naver.com>
2019-11-25 11:30:01 +0900

    Update Assign10

--- Assign10/main.py
+++ Assign10/main.py
@@ -1,20 +1,63 @@
+from queue import PriorityQueue
+
 if __name__ == "__main__":
 
-    source_data = "data.txt"
-    arrSizeList = list()
+    map = [
+        [0, 10, 3,-1,-1],
+        [10, 0, 1, 2,-1],
+        [-1, 4, 0, 8, 2],
+        [-1,-1,-1, 0, 7],
+        [-1,-1,-1, 9, 0]
+    ]
 
-    try:
-        fr = open(source_data, 'r')
+    numToAlpha = {0: "A", 1: "B", 2: "C", 3: "D", 4: "E"}
 
-        while True:
-            line = fr.readline()
-            if not line:
-                break
+    prev_d = [-1] * len(map)
+    d = [-1] * len(map)
 
-            onelineList = line.split(",")
-            arrSizeList.append(int(onelineList[0]))
-            lastValue = onelineList[1]
+    pq = PriorityQueue()
 
+    pq.put([0, 0])
 
-    except FileNotFoundError:
-        print("test File Not Found!")
+    index = 0
+
+    print("다익스트라 알고리즘으로 계산한 결과는 다음과 같습니다.")
+    print("ㅡ" * 60)
+
+    while pq.qsize() > 0:
+        item = pq.get()
+        cost = item[0]
+        vertexID = item[1]
+
+        if d[vertexID] != -1:
+            continue
+
+        d[vertexID] = cost
+
+        res = ""
+
+        res += ("-" * 110) + "\n"
+        index += 1
+
+        for i in range(0, len(map)):
+            if map[vertexID][i] >= 0:
+
+                totalCost = map[vertexID][i] + d[vertexID]
+
+                if d[i] != -1:
+                    res += "Q[" + str(i) + "] :" + "   d[" + numToAlpha[i] + "] = " + str(d[i]) + "\n"
+                    continue
+
+                res += "Q[" + str(i) + "] :" + "   d[" + numToAlpha[i] + "] = " + str(prev_d[i]) + " => " + "d[" + numToAlpha[i] + "] = " + str(totalCost) + "\n"
+
+                prev_d[i] = totalCost
+
+                pq.put([totalCost, i])
+
+        print("S[" + str(index) + "]:    d[" + numToAlpha[vertexID] + "] = " + str(d[vertexID]))
+        print(res)
+
+        print("ㅡ" * 60)
+
+    print("최종 결과: ")
+    print(d)
